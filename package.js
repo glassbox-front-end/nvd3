83cfba6be1262bb679c0c81b073e622ca7f53120
KacperBawol <kacper.bawol@glassboxdigital.com>
2025-01-30 16:25:22 +0000

    GNG-34514 migrate angularjs charts lib, angular-nvd3 to ng2-nvd3

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
 Package.describe({
     "name": 'nvd3:nvd3',
     summary: 'nvd3 based charts.',
-    version: '1.9.46',
+    version: '1.9.48',
     git: "https://github.com/glassbox-front-end/nvd3.git"
 });
 Package.on_use(function (api) {
